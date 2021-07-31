df6425290109faf8f59a2e5242660a41e47ed224
Hsy <a553331998@gmail.com>
2021-07-31 17:01:12 +0800

    完成Switch组件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,10 +1,15 @@
 <template>
-  <Switch />
+  <Switch v-model:value="bool" />
 </template>
 
 <script>
 import Switch from '../lib/Switch.vue'
+import { ref } from 'vue'
 export default {
+  setup() {
+    const bool = ref(false)
+    return { bool }
+  },
   components: { Switch },
 }
 </script>
