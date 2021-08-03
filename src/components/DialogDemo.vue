b161c420b900738075b0b8fd26b56c9c02d60921
200801 <Hsy123>
2021-08-03 19:56:55 +0800

    完成点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,9 +1,40 @@
 <template>
-  <div>Dialog 的文档</div>
+  <div>
+    <h1>Dialog 示例</h1>
+    <br />
+    <hr />
+    <h2>示例1</h2>
+    <Button @click="toggle">toggle</Button>
+    <Dialog v-model:visible="visible" :ok="confirm">
+      <template v-slot:title>
+        <strong>大傻逼</strong>
+      </template>
+      <template v-slot:content><div>左右</div> </template>
+    </Dialog>
+  </div>
 </template>
 
-<script>
-export default {}
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import { ref } from 'vue'
+export default {
+  components: { Dialog, Button },
+  setup() {
+    const visible = ref(false)
+    const toggle = () => {
+      visible.value = !visible.value
+    }
+    const confirm = () => {
+      return false
+    }
+    return {
+      visible,
+      toggle,
+      confirm,
+    }
+  },
+}
 </script>
 
 <style></style>
