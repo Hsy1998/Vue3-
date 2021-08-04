a11e7fc0af02b63878a3759c5575f9303d4ff486
Hsy <a553331998@gmail.com>
2021-08-04 23:44:19 +0800

    完成tabs点击切换

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,9 +1,33 @@
 <template>
-  <div>Tabs 的文档</div>
+  <div>
+    <h1>Tabs 示例</h1>
+    <h2>示例1</h2>
+    <br />
+    <hr />
+    <br />
+    <Tabs v-model:selected="x">
+      <Tab title="导航一">内容一</Tab>
+      <Tab title="导航二22222222222">内容二</Tab>
+    </Tabs>
+  </div>
 </template>
 
-<script>
-export default {}
+<script lang="ts">
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+import { ref } from 'vue'
+export default {
+  components: {
+    Tabs,
+    Tab,
+  },
+  setup(props) {
+    const x = ref('导航一')
+    return {
+      x,
+    }
+  },
+}
 </script>
 
 <style></style>
